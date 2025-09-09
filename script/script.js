e0028220f9beaeccd09bad6fef7e000c6c9fadb6
abdulmajed123 <abdulmajed5618@gmail.com>
2025-09-09 13:55:26 +0800

    responsive and added category function area

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 const categoryContainer = document.getElementById("categories-container");
 const cardContainer = document.getElementById("card-container");
+const cartItemsContainer = document.getElementById("cart-items-container");
 
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
@@ -33,7 +34,6 @@ const loadCategories = () => {
     });
 
   const loadPlantCategory = (id) => {
-    // console.log(id);
     const url = `https://openapi.programming-hero.com/api/category/${id}`;
     fetch(url)
       .then((res) => res.json())
@@ -46,7 +46,7 @@ const loadCategories = () => {
         cards.forEach((card) => {
           cardContainer.innerHTML += `
           <div class="bg-white max-h-full p-2 rounded-lg shadow-md space-y-2 ">
-            <img class="w-60 h-52" src="${card.image}" alt="" />
+            <img class="w-60 h-52 rounded-lg" src="${card.image}" alt="" />
             <h2 class="text-xl text-black font-semibold">${card.name}</h2>
             <p class='line-clamp-4'>
               ${card.description}
@@ -70,93 +70,16 @@ const loadCategories = () => {
         });
       });
   };
+  window.onload = () => {
+    loadPlantCategory("1");
+  };
+
+  cardContainer.addEventListener("click", (e) => {
+    // console.log(e.target.innerText);
+    if (e.target.innerText === "Add to Cart") {
+      alert("Item added to the cart");
+      console.log("Clicked Button");
+    }
+  });
 };
-
-// const loadPlantCategory = (categoryId) => {
-//   // console.log(categoryId);
-//   const url = `https://openapi.programming-hero.com/api/plant/${categoryId}`;
-//   // console.log(url);
-
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       displayPlantsCard(data.plants);
-//       console.log(data.plants);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-
-//   const displayPlantsCard = (cards) => {
-//     // console.log(cards);
-
-//     cardContainer.innerHTML = "";
-//     cards.forEach((card) => {
-//       cardContainer.innerHTML += `
-
-//            <div class="bg-white p-4 rounded-lg shadow-md space-y-2 ">
-//             <img class="w-56 h-40" src="${card.image}" alt="" />
-//             <h2 class="text-xl text-black font-semibold">${card.name}</h2>
-//             <p class=''>
-//               ${card.description}
-//             </p>
-//             <div class="flex justify-between items-center">
-//               <button
-//                 class="bg-[#DCFCE7] px-2 rounded-2xl font-semibold text-[#15803d]"
-//               >
-//                 ${card.category}
-//               </button>
-//               <div class="font-bold"><span class=" text-xl font-bold">৳</span>${card.price}</div>
-//             </div>
-//             <button
-//               class="btn btn-wide bg-[#15803d] text-xl text-white rounded-3xl"
-//             >
-//               Add to Cart
-//             </button>
-//           </div>
-
-//       `;
-//     });
-//   };
-// };
-// // loadPlantCategory(id);
 loadCategories();
-
-// const loadCards = () => {
-//   const url = `https://openapi.programming-hero.com/api/plants`;
-//   console.log(url);
-
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data.plants);
-
-//       const cards = data.plants;
-//       cards.forEach((card) => {
-//         console.log(card.plants);
-//         cardContainer.innerHTML += `
-//            <div class="bg-white p-4 rounded-lg shadow-md space-y-2 ">
-//             <img class="w-56 h-40" src="${card.image}" alt="" />
-//             <h2 class="text-xl text-black font-semibold">${card.name}</h2>
-//             <p class=''>
-//               ${card.description}
-//             </p>
-//             <div class="flex justify-between items-center">
-//               <button
-//                 class="bg-[#DCFCE7] px-2 rounded-2xl font-semibold text-[#15803d]"
-//               >
-//                 ${card.category}
-//               </button>
-//               <div class="font-bold"><span class=" text-xl font-bold">৳</span>${card.price}</div>
-//             </div>
-//             <button
-//               class="btn btn-wide bg-[#15803d] text-xl text-white rounded-3xl"
-//             >
-//               Add to Cart
-//             </button>
-//           </div>`;
-//       });
-//     });
-// };
-
-// loadCards();
